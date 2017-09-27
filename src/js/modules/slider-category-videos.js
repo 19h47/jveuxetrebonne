c83eb6a0e95c226f2660f6d19290f779e2c8a4cc
Levron Jérémy <levronjeremy@19h47.fr>
2017-09-27 19:55:47 +0200

    Update slider video

--- src/js/modules/slider-category-videos.js
+++ src/js/modules/slider-category-videos.js
@@ -27,13 +27,20 @@ SliderCategoryVideos.prototype = {
             'iconUrl': wp.current_url
         });
 
-            // console.log();
-        players && players.forEach(function (player) {
-            player.on('play', function (event) {
-    
-                players.forEach(function (player) {
-
-                    player.pause();
+        var i = 1;
+        players.forEach(function (instance) {
+            instance.on('ready', function (event) {
+                instance.getContainer().setAttribute('id', 'plyId-' + i);
+                instance.plyId = 'plyr-' + i;
+                // console.log(instance.pid);
+                i++;
+            });
+            instance.on('play', function (event) {
+                var currentPid = instance.plyId;
+                players.forEach(function (instance) {
+                    if (currentPid != instance.plyId) {
+                        instance.pause();
+                    }
                 });
             });
         });
@@ -55,7 +62,7 @@ SliderCategoryVideos.prototype = {
                 nextArrow: $slider.find('.js-slider-category-videos-next'),
                 prevArrow: $slider.find('.js-slider-category-videos-previous'),
                 mobileFirst: true,
-                // slidesToShow: 1,
+                slidesToShow: 1,
                 infinite: false,
                 initialSlide: 1,
                 
