bc9cd93d5f7a00fe75e97dc9e74e01ad7a224091
19h47 <jeremylevron@19h47.fr>
2019-03-20 12:35:02 +0100

    :pencil2: Fix typo

--- index.php
+++ index.php
@@ -1,17 +1,14 @@
 <?php
-
 /**
  * @file 		index
  * @package  	jveb
  * @author   	Jérémy Levron <jeremylevron@19h47.fr> (http://19h47.fr)
  */
 
-
 if ( ! class_exists( 'Timber' ) ) {
 	echo 'Timber not activated. Make sure you activate the plugin in <a href="/wp-admin/plugins.php#timber">/wp-admin/plugins.php</a>';
 	return;
 }
-
 
 $context = Timber::get_context();
 $post = new TimberPost();
